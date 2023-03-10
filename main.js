acd4dc35a9623a6faec53d2383501423c983d35e
aion0721 <rudolf.aion@gmail.com>
2023-03-10 23:06:09 +0900

    create init

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 // Require
 const http = require("http");
+const url = require("url");
 const fs = require("fs");
-const server = http.createServer();
 
 // Load Config
 const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
 
+// Server const
+const server = http.createServer();
+const dataDir = "./data";
+
+// Initial
+// Check data directory
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, (err) => {
+    throw err;
+  });
+  console.log("=== Initial === Create data directory.");
+}
+
+// Check resources files
+for (const resource of config.resources) {
+  const resourceFilePath = `${dataDir}/${resource}`;
+  if (!fs.existsSync(resourceFilePath)) {
+    fs.writeFileSync(resourceFilePath, "{}", (err) => {
+      throw err;
+    });
+    console.log(`=== Initial === Create resouce file(resouce: ${resource})`);
+  }
+}
+
 server.on("request", function (req, res) {
-  if (req.url === "/") {
+  const urlParse = url.parse(req.url, true);
+  if (urlParse.pathname === "/") {
     fs.readFile(__dirname + "/static/index.html", "utf8", function (err, data) {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       res.end();
     });
-  } else if (config.resources.includes(req.url.replace("/", ""))) {
+  } else if (config.resources.includes(urlParse.replace("/", ""))) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("ok");
     res.end();
