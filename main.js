a3a4423328a933c11269fea15f0122dfef91ed41
aion0721 <rudolf.aion@gmail.com>
2023-03-10 19:15:53 +0900

    create static page

---
+++ main.js
@@ -0,0 +1,28 @@
+// Require
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer();
+
+// Load Config
+const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+
+server.on("request", function (req, res) {
+  switch (req.url) {
+    case "/":
+      fs.readFile(
+        __dirname + "/static/index.html",
+        "utf8",
+        function (err, data) {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        }
+      );
+      break;
+  }
+});
+
+server.listen(config.port);
+console.log(
+  `The server has started and is listening on port number: ${config.port}`
+);
