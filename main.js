56a3ad131962f7bce307dd32683446b5aca37e5b
aion0721 <rudolf.aion@gmail.com>
2023-03-11 10:00:56 +0900

    change data array, switch request method

--- main.js
+++ main.js
@@ -15,8 +15,13 @@ const dataDir = "./data";
 init(dataDir, config.resources);
 
 server.on("request", function (req, res) {
+  // const
   const urlParse = url.parse(req.url, true);
   const reqResource = urlParse.pathname.replace("/", "");
+  const reqMethod = req.method;
+  const reqQuery = urlParse.query;
+
+  // Static Index
   if (urlParse.pathname === "/") {
     fs.readFile(__dirname + "/static/index.html", "utf8", function (err, data) {
       try {
@@ -27,23 +32,36 @@ server.on("request", function (req, res) {
         throw err;
       }
     });
+    // Check Exist Resource
   } else if (config.resources.includes(reqResource)) {
-    fs.readFile(
-      __dirname + "/" + dataDir + "/" + reqResource,
-      "utf8",
-      function (err, data) {
-        try {
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.write(data);
-          res.end();
-        } catch {
-          res.writeHead(500, { "Content-Type": "application/json" });
-          res.write("err has occured.");
-          res.end();
-          throw err;
+    // GET
+    if (reqMethod == "GET") {
+      fs.readFile(
+        __dirname + "/" + dataDir + "/" + reqResource,
+        "utf8",
+        function (err, data) {
+          try {
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.write(data);
+            res.end();
+          } catch {
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.write("err has occured.");
+            res.end();
+            throw err;
+          }
         }
-      }
-    );
+      );
+      return;
+    }
+    // POST
+    if (reqMethod == "POST") {
+    }
+
+    // DELETE
+    if (reqMethod == "DELETE") {
+    }
+    // Not found Resources
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.write("<h1>NOT FOUND...</h1>");
@@ -51,6 +69,7 @@ server.on("request", function (req, res) {
   }
 });
 
+// Server start
 server.listen(config.port);
 console.log(
   `The server has started and is listening on port number: ${config.port}`
